b1e69e2594cf03d237e702e7a143141d0b784b33
hcy <775656764@qq.com>
2020-02-02 01:27:23 +0800

    update userinfo

--- src/main/java/top/hcy/webtable/common/constant/WTokenType.java
+++ src/main/java/top/hcy/webtable/common/constant/WTokenType.java
@@ -9,7 +9,7 @@ package top.hcy.webtable.common.constant;
  **/
 public class WTokenType {
 
-    public  static  final  long  DEFAULT_TOKE = 60 * 1000;
+    public  static  final  long  DEFAULT_TOKE = 10 * 60 * 1000;
     public  static  final  long  SHORT_TOKE = 30 * 1000;
     public  static  final  long  LONG_TOKE = 120 * 1000;
 
--- src/main/java/top/hcy/webtable/service/UserInfoService.java
+++ src/main/java/top/hcy/webtable/service/UserInfoService.java
@@ -28,9 +28,60 @@ public class UserInfoService implements WService {
     @Override
     public void doService(WebTableContext ctx) {
         HashMap<String,String> map = new HashMap<>();
-
+        String routers = "[\n" +
+                "        {\n" +
+                "          \"path\": \"/Data1\",\n" +
+                "          \"children\": [\n" +
+                "            {\n" +
+                "              \"path\": \"index\",\n" +
+                "              \"name\": \"Data1\",\n" +
+                "              \"component\": \"/table/index\",\n" +
+                "              \"meta\": { \"title\": \"数据表一\", \"icon\": \"table\" }\n" +
+                "            }\n" +
+                "          ]\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"path\": \"/Data2\",\n" +
+                "          \"children\": [\n" +
+                "            {\n" +
+                "              \"path\": \"index\",\n" +
+                "              \"name\": \"Data2\",\n" +
+                "              \"component\": \"/table/index\",\n" +
+                "            \"meta\": { \"title\": \"数据表二\", \"icon\": \"table\" }\n" +
+                "            }\n" +
+                "          ]\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"path\": \"/wadmin\",\n" +
+                "          \"redirect\": \"/example/table\",\n" +
+                "          \"name\": \"Example\",\n" +
+                "          \"meta\": { \"title\": \"wadmin\", \"icon\": \"example\" },\n" +
+                "          \"children\": [\n" +
+                "            {\n" +
+                "              \"path\": \"permission\",\n" +
+                "              \"name\": \"permission\",\n" +
+                "              \"component\": \"/wadmin/permission/index\",\n" +
+                "              \"meta\": { \"title\": \"权限管理\", \"icon\": \"form\" }\n" +
+                "            },\n" +
+                "            {\n" +
+                "              \"path\": \"member\",\n" +
+                "              \"name\": \"member\",\n" +
+                "              \"component\": \"/wadmin/member/index\",\n" +
+                "              \"meta\": { \"title\": \"成员管理\", \"icon\": \"tree\" }\n" +
+                "            },\n" +
+                "            {\n" +
+                "              \"path\": \"share\",\n" +
+                "              \"name\": \"share\",\n" +
+                "              \"component\": \"/wadmin/share/index\",\n" +
+                "              \"meta\": { \"title\": \"分享管理\", \"icon\": \"nested\" }\n" +
+                "            }\n" +
+                "          ]\n" +
+                "        },\n" +
+                "        { \"path\": \"*\", \"redirect\": \"/404\", \"hidden\": true }\n" +
+                "      ]";
         map.put("name",ctx.getUsername());
         map.put("avatar",ctx.getToken());
+        map.put("routers",routers);
 
         ctx.setRespsonseEntity(map);
     }
