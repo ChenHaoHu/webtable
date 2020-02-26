14aa2b669a11c377d1d7fe951381697b01e40965
hcy <775656764@qq.com>
2020-02-26 17:03:07 +0800

    添加版本号

---
+++ src/main/java/top/hcy/webtable/VERSION.java
@@ -0,0 +1,14 @@
+package top.hcy.webtable;
+
+public final class VERSION {
+    public static final int MajorVersion = 1;
+    public static final int MinorVersion = 0;
+    public static final int RevisionVersion = 1;
+
+    public VERSION() {
+    }
+
+    public static String getVersionNumber() {
+        return "1.0.1";
+    }
+}
