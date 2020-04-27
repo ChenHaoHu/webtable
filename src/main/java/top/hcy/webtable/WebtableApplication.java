38b1feb301386ccbf0de763f3b3dae90698629e8
hcy <775656764@qq.com>
2020-04-28 01:11:13 +0800

    1. 重构数据源使用部分 2. 完成多数据源处理 3. 适配 nosql mongodb  ing

--- src/main/java/top/hcy/webtable/WebtableApplication.java
+++ src/main/java/top/hcy/webtable/WebtableApplication.java
@@ -2,8 +2,9 @@ package top.hcy.webtable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class WebtableApplication {
 
     public static void main(String[] args) {
